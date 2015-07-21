6702daf77c8a97b622d23257bfe1b6cf4607c70c
bartas <bartekkubala@wp.pl>
2015-07-21 16:31:00 +0200

    add new files

---
+++ www/jquery.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//     console.log('dupa');
+//      $(".bmw1").toggle(
+//         function(){$(this).css({"display": "block"});},
+//         function(){$('bmw1-menu').css({"color": "blue"});},
+//         function(){$('bmw1-menu').css({"color": "green"});
+//     });
+// });
